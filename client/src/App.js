41844e505894dee43aed6fd6a2061f865cf44ac2
Dianne <mdianne.gabriel@gmail.com>
2021-09-24 14:13:52 -0400

    Create and implement Goals component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
-import BattleTheme from './components/BattleTheme';
 // import Login from './components/Login';
+import BattleTheme from './components/BattleTheme';
+import Goals from './components/Goals';
 import Header from './components/Header';
 
 function App() {
@@ -8,83 +9,7 @@ function App() {
     // <Login />
     <>
       <Header />
-      <main id="main-container">
-        <section className="goals-box">
-          <article className="daily goals">
-            <h3>Daily Tasks</h3>
-            <li className="goal-entry">
-              Eat 1500 cals
-            </li>
-            <div className="goal-entry">
-              Drink woter
-            </div>
-            <div className="goal-entry">
-              Workout
-            </div>
-            <div className="goal-entry">
-              Meditate
-            </div>
-            <div className="goal-entry">
-              Solve 20 katas
-            </div>
-            <div className="goal-entry">
-              Read 10 pages of a book
-            </div>
-            <div className="goal-entry">
-              Sleep 8 hrs
-            </div>
-            <div className="goal-entry">
-              Take meds
-            </div>
-            <div className="goal-entry">
-              Eat 1500 cals
-            </div>
-            <div className="goal-entry">
-              Drink woter
-            </div>
-            <div className="goal-entry">
-              Workout
-            </div>
-            <div className="goal-entry">
-              Meditate
-            </div>
-            <div className="goal-entry">
-              Solve 20 katas
-            </div>
-            <div className="goal-entry">
-              Read 10 pages of a book
-            </div>
-            <div className="goal-entry">
-              Sleep 8 hrs
-            </div>
-            <div className="goal-entry">
-              Take meds
-            </div>
-          </article>
-
-          <article className="short goals">
-            <h3>Missions</h3>
-            <div className="goal-entry">
-              Lose 5lbs
-            </div>
-            <div className="goal-entry">
-              Go outside
-            </div>
-          </article>
-
-          <article className="long goals">
-            <h3>Quests</h3>
-            <div className="goal-entry">
-              Go to Mars
-            </div>
-            <div className="goal-entry">
-              Become president
-            </div>
-          </article>
-
-        </section>
-      </main>
-
+      <Goals />
       <BattleTheme />
     </>
   );
---
+++ client/src/components/Goals.js
@@ -0,0 +1,83 @@
+import React from "react";
+import './Goals.scss'
+
+export default function Goals() {
+  return (
+    <main id="main-container">
+      <section className="goals-box">
+        <article className="daily goals">
+          <h3>Daily Tasks</h3>
+          <li className="goal-entry">
+            Eat 1500 cals
+          </li>
+          <div className="goal-entry">
+            Drink woter
+          </div>
+          <div className="goal-entry">
+            Workout
+          </div>
+          <div className="goal-entry">
+            Meditate
+          </div>
+          <div className="goal-entry">
+            Solve 20 katas
+          </div>
+          <div className="goal-entry">
+            Read 10 pages of a book
+          </div>
+          <div className="goal-entry">
+            Sleep 8 hrs
+          </div>
+          <div className="goal-entry">
+            Take meds
+          </div>
+          <div className="goal-entry">
+            Eat 1500 cals
+          </div>
+          <div className="goal-entry">
+            Drink woter
+          </div>
+          <div className="goal-entry">
+            Workout
+          </div>
+          <div className="goal-entry">
+            Meditate
+          </div>
+          <div className="goal-entry">
+            Solve 20 katas
+          </div>
+          <div className="goal-entry">
+            Read 10 pages of a book
+          </div>
+          <div className="goal-entry">
+            Sleep 8 hrs
+          </div>
+          <div className="goal-entry">
+            Take meds
+          </div>
+        </article>
+
+        <article className="short goals">
+          <h3>Missions</h3>
+          <div className="goal-entry">
+            Lose 5lbs
+          </div>
+          <div className="goal-entry">
+            Go outside
+          </div>
+        </article>
+
+        <article className="long goals">
+          <h3>Quests</h3>
+          <div className="goal-entry">
+            Go to Mars
+          </div>
+          <div className="goal-entry">
+            Become president
+          </div>
+        </article>
+
+      </section>
+    </main>
+  );
+}
