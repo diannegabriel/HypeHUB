e3ec46bff9cb90289b38e860ef684536b89feddc
Dianne <mdianne.gabriel@gmail.com>
2021-09-24 12:57:25 -0400

    Create and implement User component

---
+++ client/src/components/User.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './User.scss'
+
+export default function Header() {
+  return (
+    <article id="user-bar">
+      <div className="avatar">
+        <div className="hexTop"></div>
+        <div className="hexBottom"></div>
+      </div>
+      <div className="user">
+        <div className="user-info">
+          <h1 className="username">Chubby_Bunny</h1>
+          <h2 className="user-level">LVL 1</h2>
+        </div>
+        <div className="exp-bar">
+          <div className="bar">
+            <div className="user-progress-line"><span className="user-exp"></span></div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
