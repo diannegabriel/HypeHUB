f42e07221847dd99e880114e1292c2b77c30d733
Dianne <mdianne.gabriel@gmail.com>
2021-09-24 17:23:50 -0400

    Install react-router. Implement sample routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
