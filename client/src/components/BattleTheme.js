f21b354c154ea37f99fbdf249f2b2ffc79d2666d
gloe2019 <gloedem@gmail.com>
2021-09-25 13:29:08 -0400

    add rudimentary player UI in component

--- client/src/components/BattleTheme.js
+++ client/src/components/BattleTheme.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./BattleTheme.scss";
 
+const track = {
+  name: "",
+  album: {
+    images: [{ url: "" }],
+  },
+  artists: [{ name: "" }],
+};
+
 export default function BattleTheme(props) {
   const [player, setPlayer] = useState(undefined);
+  // store info about currently playing track
+  const [is_paused, setPaused] = useState(false);
+  const [is_active, setActive] = useState(false);
+  const [current_track, setTrack] = useState(track);
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://sdk.scdn.co/spotify-player.js";
@@ -12,7 +24,7 @@ export default function BattleTheme(props) {
 
     window.onSpotifyWebPlaybackSDKReady = () => {
       const player = new window.Spotify.Player({
-        name: "Web Playback SDK",
+        name: "HypeHUB - BattleTheme",
         getOAuthToken: (cb) => {
           cb(props.token);
         },
@@ -29,13 +41,75 @@ export default function BattleTheme(props) {
         console.log("Device ID has gone offline", device_id);
       });
 
+      player.addListener("player_state_changed", (state) => {
+        if (!state) {
+          return;
+        }
+
+        setTrack(state.track_window.current_track);
+        setPaused(state.paused);
+
+        player.getCurrentState().then((state) => {
+          !state ? setActive(false) : setActive(true);
+        });
+      });
+
+      player.addListener(
+        "player_state_changed",
+        ({ position, duration, track_window: { current_track } }) => {
+          console.log("Currently Playing", current_track);
+          console.log("Position in Song", position);
+          console.log("Duration of Song", duration);
+        }
+      );
+
       player.connect();
     };
   }, [props.token]);
   return (
     <footer id='battle-theme'>
       <div className='container'>
-        <div className='main-wrapper'></div>
+        <div className='main-wrapper'>
+          <img
+            src={current_track.album.images[0].url}
+            className='now-playing__cover'
+            alt=''
+          />
+
+          <div className='now-playing__side'>
+            <div className='now-playing__name'>{current_track.name}</div>
+
+            <div className='now-playing__artist'>
+              {current_track.artists[0].name}
+            </div>
+          </div>
+          <button
+            className='btn-spotify'
+            onClick={() => {
+              player.previousTrack();
+            }}
+          >
+            &lt;&lt;
+          </button>
+
+          <button
+            className='btn-spotify'
+            onClick={() => {
+              player.togglePlay();
+            }}
+          >
+            {is_paused ? "PLAY" : "PAUSE"}
+          </button>
+
+          <button
+            className='btn-spotify'
+            onClick={() => {
+              player.nextTrack();
+            }}
+          >
+            &gt;&gt;
+          </button>
+        </div>
       </div>
     </footer>
   );
