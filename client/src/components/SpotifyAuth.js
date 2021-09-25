46c209c030b29aa58f03dac049a80ec879a40853
gloe2019 <gloedem@gmail.com>
2021-09-25 10:13:18 -0400

    add Spotify Authorization component

---
+++ client/src/components/SpotifyAuth.js
@@ -0,0 +1,13 @@
+export default function SpotifyAuth() {
+  return (
+    <footer>
+      <div className='spotify-auth'>
+        <header className='spotify-auth-header'>
+          <a className='btn-spotify' href='/auth/login'>
+            Login with Spotify
+          </a>
+        </header>
+      </div>
+    </footer>
+  );
+}
