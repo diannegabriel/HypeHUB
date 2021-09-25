c669a41df256abcc28b2d1d9e291b64a22329d96
gloe2019 <gloedem@gmail.com>
2021-09-25 10:20:03 -0400

    load spotify webPlayer SDK in useEffect hook

--- client/src/components/BattleTheme.js
+++ client/src/components/BattleTheme.js
@@ -1,10 +1,42 @@
-import React from "react";
-import './BattleTheme.scss'
+import React, { useEffect, useState } from "react";
+import "./BattleTheme.scss";
 
-export default function BattleTheme() {
+export default function BattleTheme(props) {
+  const [player, setPlayer] = useState(undefined);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: "Web Playback SDK",
+        getOAuthToken: (cb) => {
+          cb(props.token);
+        },
+        volume: 0.5,
+      });
+
+      setPlayer(player);
+
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+
+      player.connect();
+    };
+  }, [props.token]);
   return (
-    <footer id="battle-theme">
-      Battle Theme!
+    <footer id='battle-theme'>
+      <div className='container'>
+        <div className='main-wrapper'></div>
+      </div>
     </footer>
   );
 }
