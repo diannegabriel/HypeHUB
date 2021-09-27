ed0af5af7de5382758fdfd3e84793751afc5a530
Michael <michael.dburke56@gmail.com>
2021-09-27 09:02:08 -0400

    Make axios call to db for goals and pass into goals component.

--- client/src/hooks/useData.js
+++ client/src/hooks/useData.js
@@ -6,17 +6,20 @@ export default function useData() {
     userId: null,
     userExp: 99,
     token: null,
+    userGoals: null,
   });
 
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:5000/db/db-user"),
       axios.get("http://localhost:5000/auth/token"),
+      axios.get("http://localhost:5000/db/db-goals"),
     ]).then((all) => {
       setState((prev) => ({
         ...prev,
         userId: all[0].data.userId,
         token: all[1].data.access_token,
+        userGoals: all[2].data.goals,
       }));
     });
   }, []);
