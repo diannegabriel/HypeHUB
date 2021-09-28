678f2460b2666ede5ffc7ff179d568873c2e3284
gloe2019 <gloedem@gmail.com>
2021-09-28 13:07:38 -0400

    add missing closing div tag

--- client/src/components/GamifyModal.js
+++ client/src/components/GamifyModal.js
@@ -11,7 +11,6 @@ export default function GamifyModal(props) {
   //Rename state === lot of changes. Group talk. (App.js, dbCreateGoal.js,useData.js)
   const { state, createGoal } = useData();
 
-
   const handleSubmit = (event) => {
     event.preventDefault();
     let formData = {
@@ -35,38 +34,93 @@ export default function GamifyModal(props) {
         const type = el.dataset.rpgui;
         window.RPGUI.create(el, type);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <form onSubmit={handleSubmit} ref={formEl}>
-    <div className="Modal">
-      <div className="rpgui-content">
-        <div className="rpgui-container framed add-goal-modal">
-          <h1>Next Goal</h1>
-          <hr />
-          <label>Goal:</label>
-          <input type="text" name="goal" value={goal} autoComplete="off" onChange={(e) => setGoal(e.target.value)} />
-          <br/><br/>
-          <textarea name="description" rows="4" cols="50" placeholder="Goal Description"></textarea>
-          <br/><br/>
-          <label>Select the type:</label>
-          <select className="rpgui-dropdown" name="goal-type" data-rpgui="dropdown">
-            <option value="Daily" selected>Daily Task</option>
-            <option value="Mission">Mission</option>
-            <option value="Quest">Quest</option>
-          </select>
-          <input className="rpgui-checkbox golden" type="checkbox" value="1" name="strength" data-rpgui="checkbox" /><label>STR&nbsp;</label>
-          <input className="rpgui-checkbox golden" type="checkbox" value="1" name="vitality" data-rpgui="checkbox" /><label>VIT&nbsp;</label>
-          <input className="rpgui-checkbox golden" type="checkbox" value="1" name="knowledge" data-rpgui="checkbox" /><label>KNO&nbsp;</label>
-          <input className="rpgui-checkbox golden" type="checkbox" value="1" name="social" data-rpgui="checkbox" /><label>SOC&nbsp;</label>
-          <input className="rpgui-checkbox golden" type="checkbox" value="1" name="willpower" data-rpgui="checkbox" /><label>WIL</label>
-          <br />
-          <br />
-          <div className="rpgui-center">
-          <button className="rpgui-button" type="submit">
-            <p className="button-text">Enter</p>
-          </button>
+      <div className='Modal'>
+        <div className='rpgui-content'>
+          <div className='rpgui-container framed add-goal-modal'>
+            <h1>Next Goal</h1>
+            <hr />
+            <label>Goal:</label>
+            <input
+              type='text'
+              name='goal'
+              value={goal}
+              autoComplete='off'
+              onChange={(e) => setGoal(e.target.value)}
+            />
+            <br />
+            <br />
+            <textarea
+              name='description'
+              rows='4'
+              cols='50'
+              placeholder='Goal Description'
+            ></textarea>
+            <br />
+            <br />
+            <label>Select the type:</label>
+            <select
+              className='rpgui-dropdown'
+              name='goal-type'
+              data-rpgui='dropdown'
+            >
+              <option value='Daily' selected>
+                Daily Task
+              </option>
+              <option value='Mission'>Mission</option>
+              <option value='Quest'>Quest</option>
+            </select>
+            <input
+              className='rpgui-checkbox golden'
+              type='checkbox'
+              value='1'
+              name='strength'
+              data-rpgui='checkbox'
+            />
+            <label>STR&nbsp;</label>
+            <input
+              className='rpgui-checkbox golden'
+              type='checkbox'
+              value='1'
+              name='vitality'
+              data-rpgui='checkbox'
+            />
+            <label>VIT&nbsp;</label>
+            <input
+              className='rpgui-checkbox golden'
+              type='checkbox'
+              value='1'
+              name='knowledge'
+              data-rpgui='checkbox'
+            />
+            <label>KNO&nbsp;</label>
+            <input
+              className='rpgui-checkbox golden'
+              type='checkbox'
+              value='1'
+              name='social'
+              data-rpgui='checkbox'
+            />
+            <label>SOC&nbsp;</label>
+            <input
+              className='rpgui-checkbox golden'
+              type='checkbox'
+              value='1'
+              name='willpower'
+              data-rpgui='checkbox'
+            />
+            <label>WIL</label>
+            <br />
+            <br />
+            <div className='rpgui-center'>
+              <button className='rpgui-button' type='submit'>
+                <p className='button-text'>Enter</p>
+              </button>
+            </div>
           </div>
         </div>
       </div>
