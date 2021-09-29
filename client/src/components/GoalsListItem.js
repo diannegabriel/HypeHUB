550b06682e7fab0796420e26d7c69043d0221438
Dianne <mdianne.gabriel@gmail.com>
2021-09-28 23:11:23 -0400

    Style the goals list item components

--- client/src/components/GoalsListItem.js
+++ client/src/components/GoalsListItem.js
@@ -12,10 +12,20 @@ export default function GoalsListItem({goalId, status, title}) {
     });
   };
 
+  if (status === 'complete') {
+    status = <i class="nes-icon star"></i>;
+  } else if (status === 'incomplete') {
+    status = <i class="nes-icon star is-empty"></i>;
+  } else if (status === 'in progress') {
+    status = <i class="nes-icon star is-half"></i>;
+  }
+
   return (
     <li className="goal-entry">
-      {title}
-      <button onClick={handleClick}>{status}</button>
+      <button onClick={handleClick} className="goals-status-name">{status}</button>
+      <p className="goals-title-name">{title}</p>
+      <div className="rpgui-icon sword edit-button"></div>
+      {/* <img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/015a5440cba9551.png" className="pencil-edit-button" /> */}
     </li>
   );
 }
