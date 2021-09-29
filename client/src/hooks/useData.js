31d6c9a4f3453c053a2047ab62c846e91c9bc1aa
Michael <michael.dburke56@gmail.com>
2021-09-29 10:12:06 -0400

    Cleaned up comments

--- client/src/hooks/useData.js
+++ client/src/hooks/useData.js
@@ -16,44 +16,42 @@ let updaters = [];
 const setState = (newState) => {
   state = {
     ...state,
-    ...newState
-  }
+    ...newState,
+  };
 
-  updaters.forEach(updater => {
-    updater(state)
-  })
-}
+  updaters.forEach((updater) => {
+    updater(state);
+  });
+};
 //logout func to set state to null.
 export default function useData() {
-
   const newUpdater = useState()[1];
-  useEffect(()=>{
+  useEffect(() => {
     updaters.push(newUpdater);
-    return ()=>{
-      updaters = updaters.filter((updater) => updater !== newUpdater)
-    }
-  }, [])
-
+    return () => {
+      updaters = updaters.filter((updater) => updater !== newUpdater);
+    };
+  }, []);
 
   useEffect(() => {
-    if (!hasFetchedData){
+    if (!hasFetchedData) {
       hasFetchedData = true;
-    Promise.all([
-      axios.get("http://localhost:5000/db/db-user"),
-      axios.get("http://localhost:5000/auth/token"),
-      axios.get("http://localhost:5000/db/daily-goals"),
-      axios.get("http://localhost:5000/db/mission-goals"),
-      axios.get("http://localhost:5000/db/quest-goals"),
-    ]).then((all) => {
-      setState({
-        userId: all[0].data.userId,
-        token: all[1].data.access_token,
-        dailyGoals: all[2].data.goals,
-        missionGoals: all[3].data.goals,
-        questGoals: all[4].data.goals,
+      Promise.all([
+        axios.get("http://localhost:5000/db/db-user"),
+        axios.get("http://localhost:5000/auth/token"),
+        axios.get("http://localhost:5000/db/daily-goals"),
+        axios.get("http://localhost:5000/db/mission-goals"),
+        axios.get("http://localhost:5000/db/quest-goals"),
+      ]).then((all) => {
+        setState({
+          userId: all[0].data.userId,
+          token: all[1].data.access_token,
+          dailyGoals: all[2].data.goals,
+          missionGoals: all[3].data.goals,
+          questGoals: all[4].data.goals,
+        });
       });
-    });
-  }
+    }
   }, []);
 
   function createGoal(formData) {
@@ -66,10 +64,10 @@ export default function useData() {
     }).then(
       (res) => {
         //Update state to match db
-        const goalTypeKey = `${res.data.goal.goalType}Goals`
+        const goalTypeKey = `${res.data.goal.goalType}Goals`;
         setState({
-          [goalTypeKey]: [...state[goalTypeKey], res.data.goal]
-        })
+          [goalTypeKey]: [...state[goalTypeKey], res.data.goal],
+        });
       },
       (err) => {
         console.log(err);
@@ -77,7 +75,7 @@ export default function useData() {
     );
   }
 
-  function updateGoalStatus(data){
+  function updateGoalStatus(data) {
     //Update db with new status.
     axios({
       method: "post",
@@ -86,43 +84,27 @@ export default function useData() {
       data: JSON.stringify(data),
     }).then(
       (res) => {
-        //Update state to match db
-        // console.log(res.data);
-        const newStatus = res.data.status.status
-        const id = res.data.status.goalId
-        const goalKey = `${data.goalType}Goals`
+        const id = res.data.status.goalId;
+        const newStatus = res.data.status.status;
+        const goalKey = `${data.goalType}Goals`;
 
-        //find array of goals, same type
-        const goalsOfType = [...state[goalKey]]
         //iterate through list of goals
-        for (let i = 0; i < state[goalKey].length; i++){
-        // for (const goal of goalsOfType){
+        for (let i = 0; i < state[goalKey].length; i++) {
           //find the correct goal (goalId)
-          if (state[goalKey][i].goalId === id){
-            const pastStatus = [...state[goalKey]][i].status;
-            console.log(`pastStatus: ${pastStatus}`)
-    
-          
-          //Array of one goal type
-            const updatedArray = [...state[goalKey]]
-            updatedArray[i].status = newStatus
-            // console.log(blah)
-            // blah[i].status = newStatus;
-            // console.log(blah)
-            setState({questGoals: updatedArray}
-              // state[goalKey]: updatedArray,
-            )
+          if (state[goalKey][i].goalId === id) {
+            const updatedArray = [...state[goalKey]];
+            updatedArray[i].status = newStatus;
+
+            setState({
+              goalKey: updatedArray,
+            });
           }
         }
-        //Update status of that goal
-        //set the state
-
       },
       (err) => {
         console.log(err);
       }
     );
-
   }
 
   return { state, createGoal, updateGoalStatus };
