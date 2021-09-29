37a37001a24455abc8942734bf7b07c95931bcf0
Michael <michael.dburke56@gmail.com>
2021-09-29 18:22:19 -0400

    Update state with information returned from db update

--- server/routes/db-router.js
+++ server/routes/db-router.js
@@ -10,7 +10,7 @@ const dbReadUser = require(".././dbHelpers/users/dbReadUser");
 const dbReadGoals = require(".././dbHelpers/goals/dbReadGoals");
 const dbCreateGoal = require("../dbHelpers/goals/dbCreateGoal");
 const dbUpdateGoalStatus = require("../dbHelpers/goals/dbUpdateGoalStatus");
-const dbUpdateGoal = require ("../dbHelpers/goals/dbUpdateGoal");
+const dbUpdateGoal = require("../dbHelpers/goals/dbUpdateGoal");
 
 //create nessisary routes for db query here
 router.get("/db-user", (req, res) => {
@@ -61,24 +61,23 @@ router.post("/new-goal/", (req, res) => {
   const data = req.body;
 
   dbCreateGoal(data).then((goal) => {
-    res.json({goal})
+    res.json({ goal });
   });
 });
 
 router.post("/update-goal-status/", (req, res) => {
   const data = req.body;
   dbUpdateGoalStatus(data).then((status) => {
-    res.json({status})
+    res.json({ status });
   });
 });
 
-//put request
 router.put("/update-goal", (req, res) => {
   const data = req.body;
   // console.log(`data from body\n${JSON.stringify(data)}`)
 
-  dbUpdateGoal(data).then((status) => {
-    res.json({status})
+  dbUpdateGoal(data).then((update) => {
+    res.json({ update });
   });
 });
 
