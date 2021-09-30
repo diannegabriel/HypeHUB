93728897d68662a62388e0a01d0d5792bfac8208
gloe2019 <gloedem@gmail.com>
2021-09-29 20:17:14 -0400

    randomize quote response

--- server/routes/quote-router.js
+++ server/routes/quote-router.js
@@ -3,21 +3,35 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", (req, res) => {
-  console.log("quotes");
-  axios.get("https://zenquotes.io/api/random").then((response) => {
-    console.log(response.data);
-    let quote = response.data;
-    res.json({ quote });
-  });
+  const getRegQuote = () => {
+    axios.get("https://zenquotes.io/api/random").then((response) => {
+      let rawData = response.data;
+      let quote = rawData.find((q) => q);
+      // console.log(quote);
+      res.json({ quote });
+    });
+  };
+
+  const getTrollQuote = () => {
+    axios.get("https://api.kanye.rest").then((response) => {
+      // console.log(response.data);
+      let quote = response.data;
+      res.json({ quote });
+    });
+  };
+
+  //generate random number between 1 and 5
+  let randomNum = Math.floor(Math.random() * 5);
+
+  if (randomNum > 2) {
+    console.log(randomNum);
+    getTrollQuote();
+  } else {
+    console.log(randomNum);
+    getRegQuote();
+  }
 });
 
-router.get("/kanye", (req, res) => {
-  axios.get("https://api.kanye.rest").then((response) => {
-    console.log(response.data);
-    let kanyeQuote = response.data;
-    res.json({ kanyeQuote });
-  });
-});
 module.exports = router;
 
-// I want to randomlye call an API
+// I want to randomlye call an API depending on a random number...
