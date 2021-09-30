af6dd3c979d3d6ae3fb70add45dd7ae3c1f33c5b
Dianne <mdianne.gabriel@gmail.com>
2021-09-30 18:17:04 -0400

    Complete token system by the skills attributes list

--- client/src/components/SkillBarList.js
+++ client/src/components/SkillBarList.js
@@ -9,7 +9,10 @@ export default function SkillBarList() {
         <div id="progress" className="rpgui-container experience-bar">
           <div>
             <div id="hp-bar" data-value="0.4" className="rpgui-progress bar" data-rpguitype="progress">
-              <span className="skill-attribute-name">Strength</span>
+              <div className="skill-attribute-name">
+                Strength
+                <span>x1<div className="rpgui-icon sword strength-icon"></div></span>
+                </div>
               <div className=" rpgui-progress-track">
                 <div className="rpgui-progress-fill user-progress-line">
                   <span className="attributes user-str" style={{width: `44%`}}></span></div>
@@ -19,7 +22,10 @@ export default function SkillBarList() {
             </div>
 
             <div data-value="0.4" className="rpgui-progress bar" data-rpguitype="progress">
-              <span className="skill-attribute-name">Vitality</span>
+              <div className="skill-attribute-name">
+                Vitality
+                <span>x1<i class="nes-icon is-small heart"></i></span>
+              </div>
               <div className=" rpgui-progress-track">
                 <div className="rpgui-progress-fill user-progress-line">
                   <span className="attributes user-vit" style={{width: `30%`}}></span></div>
@@ -29,7 +35,10 @@ export default function SkillBarList() {
             </div>
 
             <div id="hp-bar" data-value="0.4" className="rpgui-progress bar" data-rpguitype="progress">
-              <span className="skill-attribute-name">Knowledge</span>
+              <div className="skill-attribute-name">
+                Knowledge
+                <span>x1<div className="rpgui-icon potion-green knowledge-icon"></div></span>
+              </div>
               <div className=" rpgui-progress-track">
                 <div className="rpgui-progress-fill user-progress-line">
                   <span className="attributes user-kno" style={{width: `69%`}}></span></div>
@@ -39,7 +48,10 @@ export default function SkillBarList() {
             </div>
 
             <div id="hp-bar" data-value="0.4" className="rpgui-progress bar" data-rpguitype="progress">
-              <span className="skill-attribute-name">Social</span>
+              <div className="skill-attribute-name">
+                Social
+                <span>x1<i class="nes-icon coin is-small"></i></span>
+              </div>
               <div className=" rpgui-progress-track">
                 <div className="rpgui-progress-fill user-progress-line">
                   <span className="attributes user-soc" style={{width: `13%`}}></span></div>
@@ -49,7 +61,10 @@ export default function SkillBarList() {
             </div>
 
             <div id="hp-bar" data-value="0.4" className="rpgui-progress bar" data-rpguitype="progress">
-              <span className="skill-attribute-name">Willpower</span>
+              <div className="skill-attribute-name">
+                Willpower
+                <span>x1<div className="rpgui-icon shield willpower-icon"></div></span>
+              </div>
               <div className=" rpgui-progress-track">
                 <div className="rpgui-progress-fill user-progress-line">
                   <span className="attributes user-wil" style={{width: `91%`}}></span></div>
