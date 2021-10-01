51835b01c2fb4517e76e11e31d5f9233bbc20301
gloe2019 <gloedem@gmail.com>
2021-10-01 12:51:51 -0400

    implement functionality to play tracks based on chosen themes

--- client/src/components/SelectTheme.js
+++ client/src/components/SelectTheme.js
@@ -1,37 +1,58 @@
 import "./SelectTheme.scss";
-import useTheme from "../hooks/useTheme"; //think of better names for this
 import { useState } from "react";
 import BattleTheme from "./BattleTheme";
 
 const SelectTheme = (props) => {
-  const [theme, setTheme] = useState(null);
-  return (
-    <footer id='select-theme'>
-      <div className='rpgui-container framed'>
-        <button className='rpgui-button' onClick={setTheme("knowledge")}>
-          <i className='fas fa-brain'></i>
-          KNOWLEDGE
-        </button>
-        <button className='rpgui-button'>
-          <i className='fas fa-address-book' onClick={setTheme("social")}></i>
-          SOCIAL
-        </button>
-        <button className='rpgui-button' onClick={setTheme("strength")}>
-          <i className='fas fa-dumbbell'></i>
-          STRENGTH
-        </button>
-        <button className='rpgui-button' onClick={setTheme("vitality")}>
-          <i className='fas fa-heartbeat'></i>
-          VITALITY
-        </button>
-        <button className='rpgui-button' onClick={setTheme("willpower")}>
-          <i className='fas fa-fist-raised'></i>
-          WILLPOWER
-        </button>
-      </div>
+  const { token } = props;
+  const [theme, setTheme] = useState();
+  console.log("selectTheme props", props);
 
-      {/* <BattleTheme theme={theme} token={props.token} /> */}
-    </footer>
+  return (
+    <>
+      {!theme ? (
+        <footer id='select-theme'>
+          <div className='rpgui-container framed'>
+            <button
+              className='rpgui-button'
+              onClick={() => setTheme("knowledge")}
+            >
+              <i className='fas fa-brain'></i>
+              KNOWLEDGE
+            </button>
+            <button className='rpgui-button'>
+              <i
+                className='fas fa-address-book'
+                onClick={() => setTheme("social")}
+              ></i>
+              SOCIAL
+            </button>
+            <button
+              className='rpgui-button'
+              onClick={() => setTheme("strength")}
+            >
+              <i className='fas fa-dumbbell'></i>
+              STRENGTH
+            </button>
+            <button
+              className='rpgui-button'
+              onClick={() => setTheme("vitality")}
+            >
+              <i className='fas fa-heartbeat'></i>
+              VITALITY
+            </button>
+            <button
+              className='rpgui-button'
+              onClick={() => setTheme("willpower")}
+            >
+              <i className='fas fa-fist-raised'></i>
+              WILLPOWER
+            </button>
+          </div>
+        </footer>
+      ) : (
+        <BattleTheme theme={theme} token={token} />
+      )}
+    </>
   );
 };
 
