22faaf3dd2a90529b64efb1a8fd9ce531cff073a
Dianne <mdianne.gabriel@gmail.com>
2021-09-30 22:45:02 -0400

    Change the modal button and the complete goal stroke

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -14,7 +14,7 @@ export default function Nav() {
     <article id="nav-bar">
       <button onClick={handleShow} className="new-goal-button">
         {/* <i className="fas fa-plus-circle fa-3x"></i> */}
-        <img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/a5f8bb0c953838d.png" className="exclamation"/>
+        <img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/88c2a3fcf8ce72e.png" className="exclamation" alt="" />
       </button>
       {/* <i className="far fa-calendar-alt fa-3x"></i> */}
 
