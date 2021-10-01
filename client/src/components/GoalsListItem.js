9c4a204e3de4991d601811ccdc9204f443fb6c35
gloe2019 <gloedem@gmail.com>
2021-10-01 02:21:28 -0400

    change class to className

--- client/src/components/GoalsListItem.js
+++ client/src/components/GoalsListItem.js
@@ -4,8 +4,13 @@ import GoalUpdate from "./GoalUpdate";
 import useData from "./../hooks/useData";
 import Collapse from "react-bootstrap/Collapse";
 
-
-export default function GoalsListItem({ goalId, status, title, goalType, goalDescription }) {
+export default function GoalsListItem({
+  goalId,
+  status,
+  title,
+  goalType,
+  goalDescription,
+}) {
   const [open, setOpen] = useState(false);
   const { updateGoalStatus, updateUserStats } = useData();
 
@@ -16,45 +21,42 @@ export default function GoalsListItem({ goalId, status, title, goalType, goalDes
       status: status,
     });
     //Set points to attr and exp on update from "in progress" to "complete"
-    if(status === "in progress"){
-      updateUserStats({
-  
-      })
+    if (status === "in progress") {
+      updateUserStats({});
     }
   };
 
   let statusIcon = "";
   if (status === "complete") {
-    statusIcon = <i className="nes-icon star"></i>;
+    statusIcon = <i className='nes-icon star'></i>;
   } else if (status === "incomplete") {
-    statusIcon = <i className="nes-icon star is-empty"></i>;
+    statusIcon = <i className='nes-icon star is-empty'></i>;
   } else if (status === "in progress") {
-    statusIcon = <i className="nes-icon star is-half"></i>;
+    statusIcon = <i className='nes-icon star is-half'></i>;
   }
 
   return (
     <>
-      <li className="goal-entry">
-        <button onClick={handleStatusClick} className="goals-status-name">
+      <li className='goal-entry'>
+        <button onClick={handleStatusClick} className='goals-status-name'>
           {statusIcon}
         </button>
-        <p className="goals-title-name tooltip">
+        <div className='goals-title-name tooltip'>
           {title}
-          <div class="tooltiptext nes-balloon">
-            <p className="tooltip-text-hover">{goalDescription}</p>
+          <div class='tooltiptext nes-balloon'>
+            <p className='tooltip-text-hover'>{goalDescription}</p>
           </div>
-        </p>
-        
+        </div>
+
         <div
-          className="rpgui-icon sword edit-button"
+          className='rpgui-icon sword edit-button'
           onClick={() => setOpen(!open)}
         ></div>
-        
       </li>
-      
+
       <Collapse in={open}>
         <div>
-          <GoalUpdate goalId={goalId} reCollapse={setOpen}/>
+          <GoalUpdate goalId={goalId} reCollapse={setOpen} />
         </div>
       </Collapse>
     </>
