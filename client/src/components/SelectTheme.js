7fe5b54ce962603b244cabeece787ef998232f5f
gloe2019 <gloedem@gmail.com>
2021-10-01 02:22:28 -0400

    attempt to set theme then render BattleTheme - this does not work...

--- client/src/components/SelectTheme.js
+++ client/src/components/SelectTheme.js
@@ -1,34 +1,36 @@
 import "./SelectTheme.scss";
 import useTheme from "../hooks/useTheme"; //think of better names for this
-const setTheme = () => {
-  //useTheme hook to set state...
-};
+import { useState } from "react";
+import BattleTheme from "./BattleTheme";
 
-const SelectTheme = () => {
+const SelectTheme = (props) => {
+  const [theme, setTheme] = useState(null);
   return (
     <footer id='select-theme'>
       <div className='rpgui-container framed'>
-        <button className='rpgui-button'>
+        <button className='rpgui-button' onClick={setTheme("knowledge")}>
           <i className='fas fa-brain'></i>
           KNOWLEDGE
         </button>
         <button className='rpgui-button'>
-          <i className='fas fa-address-book'></i>
+          <i className='fas fa-address-book' onClick={setTheme("social")}></i>
           SOCIAL
         </button>
-        <button className='rpgui-button'>
+        <button className='rpgui-button' onClick={setTheme("strength")}>
           <i className='fas fa-dumbbell'></i>
           STRENGTH
         </button>
-        <button className='rpgui-button'>
+        <button className='rpgui-button' onClick={setTheme("vitality")}>
           <i className='fas fa-heartbeat'></i>
           VITALITY
         </button>
-        <button className='rpgui-button'>
+        <button className='rpgui-button' onClick={setTheme("willpower")}>
           <i className='fas fa-fist-raised'></i>
           WILLPOWER
         </button>
       </div>
+
+      {/* <BattleTheme theme={theme} token={props.token} /> */}
     </footer>
   );
 };
