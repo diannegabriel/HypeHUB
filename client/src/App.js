9a1b83d0f58ddbdaefc47341d48c76ff361a094f
gloe2019 <gloedem@gmail.com>
2021-10-01 12:50:29 -0400

    cleanup...

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.scss";
 import SpotifyAuth from "./components/SpotifyAuth";
-import BattleTheme from "./components/BattleTheme";
 import Goals from "./components/Goals";
 import Header from "./components/Header";
 import GamifyLogin from "./components/GamifyLogin";
@@ -31,14 +29,7 @@ function App() {
         </>
       )}
 
-      {state.token ? <BattleTheme token={state.token} /> : <SpotifyAuth />}
-      {/* {state.token ? <SelectTheme token={state.token} /> : <SpotifyAuth />} */}
-      {/* Can I initialize a component with an onClick? - I tried setting the theme then rendering -- no go... */}
-      {/* once authenticated, I want to show the select theme, from there I want to be able to set state and pass that in as props to the battletheme component... */}
-      {/* !theme ? <SelectTheme /> : <BattleTheme token={state.token} theme={..theme} /> */}
-      {/* <SelectTheme /> */}
-      {/* </div>
-    </div> */}
+      {state.token ? <SelectTheme token={state.token} /> : <SpotifyAuth />}
     </>
   );
 }
